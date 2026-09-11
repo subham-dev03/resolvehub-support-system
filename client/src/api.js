a7056a4130e3999f50097196f7aa6553abbcf985
subham-dev03 <2003subhamghosh2003@gmail.com>
2026-09-11 21:13:44 +0530

    Connect frontend to deployed backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+// const api = axios.create({ baseURL: "http://localhost:5000/api" });
+
+const api = axios.create({
+  baseURL: "https://resolvehub-backend-ns1u.onrender.com/api"
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("helpdesk_token");
